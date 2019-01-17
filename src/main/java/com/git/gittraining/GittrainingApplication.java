ec104e97a370aa80b3790949f0bf81ec01b76376
sanny <sannypathak15@gmail.com>
2019-01-17 12:16:06 +0530

    First commit

---
+++ src/main/java/com/git/gittraining/GittrainingApplication.java
@@ -0,0 +1,14 @@
+package com.git.gittraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittrainingApplication.class, args);
+	}
+
+}
+
